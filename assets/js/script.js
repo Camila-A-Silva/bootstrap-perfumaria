0801d8e1bd730959b5e9145bfa35dc92538600aa
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-05-14 11:29:37 -0300

    Tentando formatar os cards
    Co-authored-by: Lorena Gonçalves <lorenagoncalves02@users.noreply.github.com>
    Co-authored-by: PietrogSouza <PietrogSouza@users.noreply.github.com>
    Co-authored-by: Helena-Rosa <Helena-Rosa@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
+// Alerta com Input de email
+document.getElementById('btn-contato').addEventListener('click', function(){
+  Swal.fire({
+    title: 'Digite seu email',
+    input: 'email',
+    inputPlaceholder: 'Endereço de e-mail inválido',
+    showCancelButton: true,
+    confirmButtonText: 'Enviar',
+    cancelButtonText: 'Cancelar'
+  }).then((email) =>{
+    if (email.isConfirmed && email.value){
+      Swal.fire('Verifique seu email', 'success');
+    } 
+  })
+});
+
+
+// Alerta Sucesso
+document.getElementById('btn-comprar').addEventListener('click', function(){
+  Swal.fire('Compra realizada', 'success');
+})
+
+
+
+
+
 // Alerta com Prompt de Input
-  document.getElementById('alertInput').addEventListener('click', function() {
+document.getElementById('alertInput').addEventListener('click', function() {
     Swal.fire({
       title: 'Digite seu nome',
       input: 'text',
@@ -9,7 +35,18 @@
       cancelButtonText: 'Cancelar'
     }).then((result) => {
       if (result.isConfirmed && result.value) {
-        Swal.fire(`Olá, ${result.value}!`, 'Seu nome foi recebido.', 'success');
+        Swal.fire(`Olá, ${result.value}!`, 'Cadastro concluido.', 'success');
       }
     });
   });
+
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+});
+
+
+
